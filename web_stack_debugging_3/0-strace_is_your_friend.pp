fab9584c38e27b85a868d508d82b50d20829cd58
Your Name <you@example.com>
2026-04-04 06:04:50 -0700

    Placed Puppet manifest and README in web_stack_debugging_3

--- web_stack_debugging_3/0-strace_is_your_friend.pp
+++ web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,4 +1,4 @@
-# Fixes bad "phpp" extension to "php" in the WordPress file wp-settings.php
+# Fixes bad "phpp" extension to "php" in the WordPress file 'wp-settings.php'.
 
 exec { 'fix-wordpress':
   command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
